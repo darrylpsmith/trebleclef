ac992d4e1e75987d5e05addda0a41ef0199c2e52
darrylpsmith <darryl.smith@sage.com>
2015-09-22 18:28:19 +0200

    error traps in main function

--- Scripts/TrebleClefMainProgramFunctions.js
+++ Scripts/TrebleClefMainProgramFunctions.js
@@ -5,29 +5,44 @@ function Main() {
 
     makeMusicScore();
 
-    // Retrieve stored settings
-    var speed = localStorage.getItem("musicscorespeed");
 
-    var lownote = localStorage.getItem("lownote");
-    var highnote = localStorage.getItem("highnote");
-    if (speed == "") {
-        speed = "3";
+    try {
+
+        // Retrieve stored settings
+        speed = localStorage.getItem("musicscorespeed");
+
+        lownote = localStorage.getItem("lownote");
+        highnote = localStorage.getItem("highnote");
+
+        if ((speed == "") || (speed == "0") || (speed == null)) {
+            speed = "3";
+        }
+
+        if ((lownote == null) || (lownote == "") || (lownote == "0")) {
+            lownote = "1";
+        }
+
+        if ((highnote == null) || (highnote == "") || (highnote == "0")) {
+            highnote = "25";
+        }
+
+        document.getElementById("speed").value = speed;
+
+        document.getElementById("lownote").value = lownote;
+        document.getElementById("highnote").value = highnote;
+
+    } catch (e) {
+        alert(e);
     }
 
-    if ((lownote == "") || (lownote == "0")) {
-        lownote = "1";
+
+    try {
+        changeSpeed();
+        changeHighest();
+        changeLowest();
+    } catch (e) {
+        alert(e);
     }
 
-    if ((highnote == "") || (highnote == "0")) {
-        highnote = "25";
-    }
-
-    document.getElementById("speed").value = speed;
-
-    document.getElementById("lownote").value = lownote;
-    document.getElementById("highnote").value = highnote;
-    changeSpeed();
-    changeHighest();
-    changeLowest();
 
 };
