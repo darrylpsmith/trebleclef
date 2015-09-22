544e2294ae8cf6ab19628f469d6c8d52d14b1b07
darrylpsmith <darryl.smith@sage.com>
2015-09-22 18:03:14 +0200

    added Main javascript file

---
+++ Scripts/TrebleClefMainProgramFunctions.js
@@ -0,0 +1,33 @@
+﻿
+function Main() {
+
+
+
+    makeMusicScore();
+
+    // Retrieve stored settings
+    var speed = localStorage.getItem("musicscorespeed");
+
+    var lownote = localStorage.getItem("lownote");
+    var highnote = localStorage.getItem("highnote");
+    if (speed == "") {
+        speed = "3";
+    }
+
+    if ((lownote == "") || (lownote == "0")) {
+        lownote = "1";
+    }
+
+    if ((highnote == "") || (highnote == "0")) {
+        highnote = "25";
+    }
+
+    document.getElementById("speed").value = speed;
+
+    document.getElementById("lownote").value = lownote;
+    document.getElementById("highnote").value = highnote;
+    changeSpeed();
+    changeHighest();
+    changeLowest();
+
+};
